0eba885ec95705d1e8f750cd392e02e1d8ab39db
John P <90646213+Onotate@users.noreply.github.com>
2022-07-31 11:08:59 -0400

    listings: created ListingMethod class.

--- src/mybnb/ListingMethods.java
+++ src/mybnb/ListingMethods.java
@@ -1,2 +1,20 @@
-package mybnb;public class ListingMethods {
+package mybnb;
+
+public class ListingMethods {
+    public ListingMethods() {
+        super();
+    }
+
+    public void addListing() {
+
+    }
+
+    public void updateListing() {
+
+    }
+
+    public void removeListing() {
+
+    }
+
 }
