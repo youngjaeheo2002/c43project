21df856536ac0d6fe83017607289673a13e6a2b0
youngjaeheo2002 <youngjaeheo2002@gmail.com>
2022-08-01 02:50:21 -0400

    first draft on all the account operations/fixed the signup to work with last name and first name

--- src/mybnb/AccountMethod.java
+++ src/mybnb/AccountMethod.java
@@ -3,33 +3,27 @@ package mybnb;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class AccountMethod {
-    Connection connection;
-
-    public AccountMethod() {
-        try {
-            this.connection = SQLdriver.getConnection();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+public class AccountMethod extends Methods{
 
     // Write operation related queries here
 
-    public void SignUp(String username, String password,Date dob, String address,String occupation,int sin){
+    //0 for success -1 for failure
+    public int SignUp(String username, String password,String last_name, String first_name,Date dob, String address,String occupation,int sin){
         try{
            
-            PreparedStatement account = connection.prepareStatement("INSERT INTO accounts (username,password,dob,address,occupation,sin) VALUES ( '"+username+"','"+password+"','"+address+"', '"+occupation+"', '"+sin+"')");
+            PreparedStatement account = connection.prepareStatement("INSERT INTO accounts (username,password,last_name,first_name,dob,address,occupation,sin) VALUES ( '"+username+"','"+password+"','"+last_name+"','"+first_name+"',,'"+dob+"''"+address+"', '"+occupation+"', '"+sin+"')");
             account.executeUpdate();
+
         }
 
         catch(Exception e){
             System.out.println(e);
-
+            return -1;
         }
         finally{
             System.out.println("SignUp Completed");
         }
+        return 0;
     }
 
     public boolean Login(String username,String password) throws Exception{
@@ -43,16 +37,123 @@ public class AccountMethod {
                 array.add (result.getString("username"));
                
             }
+            System.out.println("Login complete");
             return !(array.isEmpty());
+        }
+
+        catch(Exception e){
+            System.out.println(e);
+            return false;
+        }
+
+        finally{
+            
+        }
+    }
+
+    public ResultSet getCreditCards(String username) throws Exception{
+        try{
+            PreparedStatement creditCard = connection.prepareStatement("SELECT c.cardId, c.card_num, c.expiry_date, c.card_type FROM creditCards c, accounts a WHERE a.uid = c.renterId AND a.username = '"+username+"'");
+            ResultSet result = creditCard.executeQuery();
+            
+            return result;
+        }
+
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+
+        finally{
+            System.out.println("All credit cards have been selected");
+        }
+
+    }
+
+    public void addCreditCard(int card_num, String card_type,Date expiry_date, int renterId) throws Exception{
+        try{
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO creditCards (card_num,card_type,expiry_date,renterId) VALUES ('"+card_num+"','"+card_type+"','"+expiry_date+"','"+renterId+"')");
+            
+            statement.executeUpdate();
+        }
+
+        catch(Exception e){
+            System.out.println(e);
+
+        }
+
+        finally{
+            System.out.println("Credit Card added");
+        }
+    }
+
+    public ResultSet getAccountInfo(String username){
+        try{
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM accounts WHERE username = '"+username+"'");
+            return statement.executeQuery();
+        }
+
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+
+        finally{
+            System.out.println("Got Account info");
+        }
+    }
+
+    public boolean isaHost(int id){
+        try{
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM listings WHERE hostId = '"+id+"'");
+            ResultSet result = statement.executeQuery();
+            return result.next();
         }
 
         catch(Exception e){
             System.out.println(e);
         }
 
-        finally{
-            System.out.println("Login complete");
-        }
         return false;
     }
+
+    public boolean isaRenter(int id){
+        try{
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM creditCards WHERE renterId = '"+id+"'");
+            return statement.executeQuery().next();
+        }
+
+        catch(Exception e){
+            System.out.println(e);
+
+        }
+         return false;
+        
+    }
+
+    public ResultSet getBookings(int id){
+        try{
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM bookings WHERE renterId = '"+id+"'");
+            return statement.executeQuery();
+        }
+
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public ResultSet getListings(int id){
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM listings WHERE hostId = '"+id+"'");
+            return statement.executeQuery();
+        }
+
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+
+
 }
