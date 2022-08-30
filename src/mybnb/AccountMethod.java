e896c519758eaf8d417adeea1ec9fd155a2e60f5
youngjaeheo2002 <youngjaeheo2002@gmail.com>
2022-08-30 00:50:10 -0400

    made a random comment

--- src/mybnb/AccountMethod.java
+++ src/mybnb/AccountMethod.java
@@ -7,7 +7,8 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.*;
 import java.util.ArrayList;
-
+ //comment for the skae of commenting
+ 
 public class AccountMethod extends Methods{
 
     // Write operation related queries here
